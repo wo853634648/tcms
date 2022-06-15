7cb20e457a4f4cede0bb8b99f12e3a61393264c8
wu <853634648@qq.com>
2022-06-16 00:33:13 +0800

    初始化

---
+++ src/main/java/com/wu/tcmsboot/TcmsBootApplication.java
@@ -0,0 +1,13 @@
+package com.wu.tcmsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmsBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcmsBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wu/tcmsboot/TcmsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wu.tcmsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmsBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
